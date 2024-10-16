c0ab8f8ca737a0f5980a03c26c5d9f2a13a0dded
facu911 <facundolpal@gmail.com>
2024-10-15 23:19:41 -1200

    validacion de campos vacios

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const boton=document.querySelector(".button");
 const formulario=document.querySelector(".form");
 const fotoPerfil=document.querySelector(".fotoPerfil");
 const container=document.querySelector(".container-log")
+const password=document.querySelector(".password");
+const email=document.querySelector(".email");
+const warner1=document.querySelector(".warner1");
+const warner2=document.querySelector(".warner2");
+const warner3=document.querySelector(".warner3");
+const welcomeTitle=document.querySelector(".title");
+
 boton.addEventListener("click",()=>{
-    fotoPerfil.classList.add("disappear");
-    formulario.classList.add("disappear");
-    container.classList.add("welcome");
-    container.innerHTML+=`<h1 class="title">BIENVENIDO!</h1>`
+    if(password.value.length<1&&email.value.length<1){
+        warner1.classList.remove("disable");
+    }else if(password.value.length>0&&email.value.length<1){
+        warner2.classList.remove("disable");
+    }else if(password.value.length<1&&email.value.length>0){
+        warner3.classList.remove("disable");
+    }else if(password.value.length>0&&email.value.length>0){
+        fotoPerfil.classList.add("disable");
+        formulario.classList.add("disable");
+        container.classList.add("welcome");
+        welcomeTitle.classList.remove("disable")
+    }
 })
